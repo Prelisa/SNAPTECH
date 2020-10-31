761c24f4ab66f1a22abcca4f4468a4b5e982e90e
Prelisa <dprelisa@gmail.com>
2020-10-31 23:07:15 +0545

    carousel

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var i;
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
-    const panel = accordion.nextElementSibling;
+    const panel = acc.nextElementSibling;
     if (panel.classList.contains('active')) {
       panel.style.maxHeight = panel.scrollHeight + 'px';
     }
@@ -50,22 +50,47 @@ for (i = 0; i < productDetail.length; i++) {
 }
 
 
-// var mobileNav = document.getElementsByClassName('js--mobile-nav-icon');
+function testimonialbtncontainer(value) {
+  var testimonialbutton = document.getElementsByClassName('testimonial-slider-button');
+  var cardcontainer = document.getElementsByClassName('blocks');
 
-// mobileNav.addEventListener("click", function () {
+  if (value == '1') {
+    cardcontainer[0].style.left = '0%';
+    testimonialbutton[0].style.background = "#00ff57";
+    testimonialbutton[1].style.background = "#ecf0f3";
+    testimonialbutton[2].style.background = "#ecf0f3";
+
+    console.log('card 1 selected')
+
+  }
+  if (value == '2') {
+    cardcontainer[0].style.left = '-70%';
+    testimonialbutton[1].style.background = "#00ff57";
+    testimonialbutton[0].style.background = "#ecf0f3";
+    testimonialbutton[2].style.background = "#ecf0f3";
+
+    console.log('card 2 selected')
+
+  }
+  if (value == '3') {
+    cardcontainer[0].style.left = '-90%';
+    testimonialbutton[2].style.background = "#00ff57";
+    testimonialbutton[1].style.background = "#ecf0f3";
+    testimonialbutton[0].style.background = "#ecf0f3";
+
+    console.log('card 3 selected')
+
+  }
+}
 
 
-//   var nav = $('.js--main-nav');
-//   var icon = $('.js--mobile-nav-icon i')
-//   nav.slideToggle(200);
-
-//   if (icon.hasClass('ion-navicon-round')) {
-//     icon.addClass('ion-close-round')
-//     icon.removeClass('ion-navicon-round')
-//   }
-//   else {
-//     icon.addClass('ion-navicon-round')
-//     icon.removeClass('ion-close-round');
-//   }
-
-// });
+$(function ()  {
+  $('.multiple-items').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows:false,
+    infinite:true
+  });
+});
