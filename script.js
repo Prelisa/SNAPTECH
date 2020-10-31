7ea81d3e22d7d2d2f5208faf314858c55f421b2b
Prelisa <dprelisa@gmail.com>
2020-11-01 01:02:30 +0545

    nav responsive

--- script.js
+++ script.js
@@ -50,39 +50,6 @@ for (i = 0; i < productDetail.length; i++) {
 }
 
 
-function testimonialbtncontainer(value) {
-  var testimonialbutton = document.getElementsByClassName('testimonial-slider-button');
-  var cardcontainer = document.getElementsByClassName('blocks');
-
-  if (value == '1') {
-    cardcontainer[0].style.left = '0%';
-    testimonialbutton[0].style.background = "#00ff57";
-    testimonialbutton[1].style.background = "#ecf0f3";
-    testimonialbutton[2].style.background = "#ecf0f3";
-
-    console.log('card 1 selected')
-
-  }
-  if (value == '2') {
-    cardcontainer[0].style.left = '-70%';
-    testimonialbutton[1].style.background = "#00ff57";
-    testimonialbutton[0].style.background = "#ecf0f3";
-    testimonialbutton[2].style.background = "#ecf0f3";
-
-    console.log('card 2 selected')
-
-  }
-  if (value == '3') {
-    cardcontainer[0].style.left = '-90%';
-    testimonialbutton[2].style.background = "#00ff57";
-    testimonialbutton[1].style.background = "#ecf0f3";
-    testimonialbutton[0].style.background = "#ecf0f3";
-
-    console.log('card 3 selected')
-
-  }
-}
-
 
 $(function ()  {
   $('.multiple-items').slick({
