3047dbc6bf57d0a9a1c5febd7ceb38dbd102fe15
Prelisa <dprelisa@gmail.com>
2020-10-31 23:48:17 +0545

    carousel to brands

--- script.js
+++ script.js
@@ -94,3 +94,14 @@ $(function ()  {
     infinite:true
   });
 });
+
+$(function ()  {
+  $('.multiple-images').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows:false,
+    infinite:true
+  });
+});
