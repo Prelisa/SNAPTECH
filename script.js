79674896f3832c093fe4e97a32bdf0bfe0b08971
Prelisa <dprelisa@gmail.com>
2020-11-19 00:29:58 +0545

    about page responsive complete

--- script.js
+++ script.js
@@ -51,25 +51,26 @@ for (i = 0; i < productDetail.length; i++) {
 
 
 
-$(function ()  {
+$(function () {
   $('.multiple-items').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
-    arrows:false,
-    infinite:true
+    arrows: false,
+    infinite: true
+
   });
 });
 
-$(function ()  {
+$(function () {
   $('.multiple-images').slick({
     slidesToShow: 5,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
-    arrows:false,
-    infinite:true,
+    arrows: false,
+    infinite: true,
     responsive: [
       {
         breakpoint: 1200,
@@ -86,81 +87,97 @@ $(function ()  {
         }
       },
       {
-        breakpoint:750,
-        settings:{
-          slidesToShow:2,
-          slidesToScroll:1
+        breakpoint: 750,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
         }
       },
       {
-        breakpoint:555,
-        settings:{
-          slidesToShow:2,
-          slidesToScroll:1
+        breakpoint: 555,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
         }
       }
     ]
-    
+
   });
 });
 
-$(function ()  {
+$(function () {
   $('.multiple-items-team').slick({
     slidesToShow: 3,
     slidesToScroll: 3,
     autoplay: true,
     autoplaySpeed: 5000,
-    arrows:false,
-    infinite:true
+    arrows: false,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 950,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 });
 
-$('.previous').click(function(){
+$('.previous').click(function () {
   $('.multiple-images').slick('slickPrev');
- 
+
 })
-$('.next').click(function(){
+$('.next').click(function () {
   $('.multiple-images').slick('slickNext');
 })
 
 $('.owl-carousel').owlCarousel({
-  loop:true,
-  autoplay:true,
-  autoplaySpeed:3000,
-  nav:false,
-  dots:true,
+  loop: true,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  nav: false,
+  dots: true,
   // autoplayHoverPause: true,
-  responsive:{
-      0:{
-          items:1
-      },
-      600:{
-          items:1
-      },
-      1000:{
-          items:1
-      }
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 1
+    },
+    1000: {
+      items: 1
+    }
   }
 })
 
 var owl = $('.owl-carousel');
 owl.owlCarousel();
 // Listen to owl events:
-owl.on('changed.owl.carousel', function(event) {
+owl.on('changed.owl.carousel', function (event) {
   var currentItem = event.item.index;
-  window.location.hash = currentItem + 1;  
+  window.location.hash = currentItem + 1;
   // console.log(currentItem);  
-  if(currentItem.toString() == '2'){
+  if (currentItem.toString() == '2') {
     $("#Sbtn2").addClass("active");
     $("#Sbtn3").removeClass("active");
     $("#Sbtn4").removeClass("active");
   }
-  else if(currentItem.toString() == '3'){
+  else if (currentItem.toString() == '3') {
     $("#Sbtn3").addClass("active");
     $("#Sbtn2").removeClass("active");
     $("#Sbtn4").removeClass("active");
   }
-  else if(currentItem.toString() == '4'){
+  else if (currentItem.toString() == '4') {
     $("#Sbtn4").addClass("active");
     $("#Sbtn3").removeClass("active");
     $("#Sbtn2").removeClass("active");
@@ -168,24 +185,24 @@ owl.on('changed.owl.carousel', function(event) {
 })
 
 
-$(document).ready(function() {
-  $(".Sbtn1").click(function(){
+$(document).ready(function () {
+  $(".Sbtn1").click(function () {
     owl.trigger('to.owl.carousel', [2, 1000]);
     $("#Sbtn2").addClass("active");
     $("#Sbtn3").removeClass("active");
     $("#Sbtn4").removeClass("active");
-   
-  }); 
-  $(".Sbtn2").click(function(){
+
+  });
+  $(".Sbtn2").click(function () {
     owl.trigger('to.owl.carousel', [3, 1000]);
     $("#Sbtn3").addClass("active");
     $("#Sbtn2").removeClass("active");
     $("#Sbtn4").removeClass("active");
-  }); 
-  $(".Sbtn3").click(function(){
-    owl.trigger('to.owl.carousel', [4,1000]);
+  });
+  $(".Sbtn3").click(function () {
+    owl.trigger('to.owl.carousel', [4, 1000]);
     $("#Sbtn4").addClass("active");
     $("#Sbtn3").removeClass("active");
     $("#Sbtn2").removeClass("active");
-  }); 
+  });
 });
